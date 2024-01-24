cc35df2089fc5bba16abfb4193611e07e4dc2045
Prasad Shelke <shelkeprasad1102@gmail.com>
2024-01-24 13:50:30 +0530

    display commit

--- app/javascript/components/Todos/DisplayTodo.jsx
+++ app/javascript/components/Todos/DisplayTodo.jsx
@@ -21,6 +21,7 @@ const DisplayTodo = (props) => {
           <p>Body: {todos.body}</p>
           <p>Created At: {todos.created_at}</p>
           <p>Updated At: {todos.updated_at}</p>
+          <p>Updated At: {todos.updated_at}</p>
         </div>
       )}
       <Link to="/">Home</Link>
